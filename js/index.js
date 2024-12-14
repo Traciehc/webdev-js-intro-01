2a25c92041466d0d91d4c72aa6230b9e3bf0e64c
Tracie Childers <traciechilders01@gmail.com>
2024-12-13 21:14:48 -0500

    modified README.md commented out js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/*"use strict"
+
+const number = 56;
+console.log(number);
+
+console logged 56
+
+const Firstname = "Tracie";
+console.log(Firstname);
+
+console logged Tracie
+
+const number = 57;
+console.log(number);
+
+console logged   Uncaught syntex error:  redeclaration of const number;
+
+let supper = "Lasagna";
+console.log (supper);
+
+console logged Lasagna
+
+let supper = "Spaghetti";
+console.log(supper);
+
+console logged Uncaught syntex error:  redeclaration of let supper
+
+supper = "Spaghetti";
+console.log(supper);
+
+console logged Spaghetti
+
+*/
+
+
