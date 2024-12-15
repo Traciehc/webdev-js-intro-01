9c675f1e8385b3c91317eed3d76b586261a114ce
Tracie Childers <traciechilders01@gmail.com>
2024-12-14 19:44:32 -0500

    updated README.md completed js/assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -9,12 +9,13 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
+const book = "Little Women";
 
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
 
-
+let activity = "Singing";
 
 function updateBookSentence() {
     // Sets the text inside the element with the id of book answer. 
@@ -25,12 +26,12 @@ function updateBookSentence() {
 function updateActivitySentence() {
     // Update this function 
     // (hint: it will be similiar to the previous function)
-
+activityAnswerEl.innerText = activity;
 }
 
 function render() {
     // Finish writing this function
-    updateBookSentence();
+    updateBookSentence("This is an Awesome Book!");
 
 }
 
